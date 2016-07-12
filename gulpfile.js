ee18d39894de8634a1fb440548348bf221633e7b
MauriceStark <maurice222@outlook.com>
2016-07-12 18:15:52 -0500

    Server with live-reloading

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,38 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
 var PATH = {
-  less: './application/src/less/*.less',
-  css: './application/dist/css'
+  less: './application/src/less/theme.less',
+  css: './application/dist/css',
+  baseDir:  './application'
 };
 
 gulp.task('less', function () {
   return gulp.src(PATH.less)
     .pipe(less())
-    .pipe(gulp.dest(PATH.css));
+    .pipe(gulp.dest(PATH.css))
+});
+
+// Watch scss AND html files, doing different things with each.
+gulp.task('server', function () {
+  // Serve files from the root of this project
+  browserSync.init({
+    server: {
+      baseDir: PATH.baseDir
+    }
+  });
 });
 
 gulp.task('watch', function () {
   gulp.watch(PATH.less, ['less']);
+  gulp.watch(PATH.css + "/*.css").on('change', reload);
+  gulp.watch(PATH.baseDir + "/*.html").on('change', reload);
 });
 
-gulp.task('build', ['less', 'watch']);
+gulp.task('build', ['less', 'server', 'watch']);
 
 // run "gulp"
 gulp.task('default', ['build']);
